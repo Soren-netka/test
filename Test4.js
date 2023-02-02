f3c78ea76f838c740abf29fb281342f8a401d5ac
Soren-netka <75098777+Soren-netka@users.noreply.github.com>
2023-02-02 06:13:55 -0500

    4

--- Test4.js
+++ Test4.js
@@ -1,4 +1,4 @@
-fetch("https://reqbin.com/echo/post/json", {
+fetch("http://offsec-chalbroker.osiris.cyber.nyu.edu:12345/note/new", {
 method: 'POST',
 headers: {
   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
